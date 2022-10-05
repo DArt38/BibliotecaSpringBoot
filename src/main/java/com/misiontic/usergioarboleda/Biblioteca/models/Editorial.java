3519d8224154c6b832d16c3bdfd60e4834d6c867
DArt38 <danielararat@unicomfacauca.edu.co>
2022-10-04 20:47:09 -0500

    Add attributes,constructors and basic tags to the models

--- src/main/java/com/misiontic/usergioarboleda/Biblioteca/models/Editorial.java
+++ src/main/java/com/misiontic/usergioarboleda/Biblioteca/models/Editorial.java
@@ -1,4 +1,35 @@
 package com.misiontic.usergioarboleda.Biblioteca.models;
 
-public class Editorial {
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
+import org.springframework.data.annotation.Id;
+import org.springframework.relational.core.mapping.Table;
+
+
+@Entity
+@Table (name = "editorial")
+public class Editorial implements Serializable {
+
+    //Atributos
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String country;
+
+
+    /**
+     *
+     * @param id
+     * @param name
+     * @param country
+     */
+    public Editorial(Integer id,String name,String country){
+        this.id = id;
+        this.name = name;
+        this.country = country;
+
+    }
 }

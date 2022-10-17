04029167a335e4fc634880239a97b74d77b4110c
DArt38 <danielararat@unicomfacauca.edu.co>
2022-10-17 01:01:59 -0500

    BibliotecaApplication changes

--- src/main/java/com/misiontic/usergioarboleda/Biblioteca/BibliotecaApplication.java
+++ src/main/java/com/misiontic/usergioarboleda/Biblioteca/BibliotecaApplication.java
@@ -2,13 +2,11 @@ package com.misiontic.usergioarboleda.Biblioteca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 
-@EnableConfigurationProperties
-@EntityScan(basePackages = {"com.misiontic.usergioarboleda.Biblioteca.models"})
-@SpringBootApplication
+
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class BibliotecaApplication {
 
 	public static void main(String[] args) {
